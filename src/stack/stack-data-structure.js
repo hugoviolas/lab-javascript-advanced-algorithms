d3bfbe01a34441b898e18b55c8b025e7c637415c
Hugo Violas <violas.hugo@gmail.com>
2022-10-15 18:44:50 +0200

    added stack part

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,48 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length === 0) return true;
+    return false;
   }
 
   push(item) {
-    // ... your code goes here
+    if (!this.canPush) {
+      throw new Error("STACK_OVERFLOW");
+    }
+    try {
+      this.stackControl[this.stackControl.length] = item;
+      return this.stackControl;
+    } catch (error) {
+      return error;
+    }
   }
 
   pop() {
-    // ... your code goes here
+    let returnedElement;
+    if (this.isEmpty) {
+      returnedElement = this.stackControl.slice(
+        this.stackControl.length - 1,
+        this.stackControl.length
+      )[0];
+    } else {
+      throw new Error("STACK_UNDERFLOW");
+    }
+    return returnedElement;
   }
 
   display() {
-    // ... your code goes here
-  }  
+    console.log(this.stackControl);
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
